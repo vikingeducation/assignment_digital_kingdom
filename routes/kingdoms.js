b16c38241719a1516bf9d785a6c0be3f239bd91a
William Whitworth <william10charles@gmail.com>
2017-03-31 10:44:55 -0600

    completing getter methods

--- routes/kingdoms.js
+++ routes/kingdoms.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { getKingdoms } = require("../services/kingdom-store");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  console.log(getKingdoms());
   res.render("kingdoms", {});
 });
 
-router.get('/:name', (req, res) => {
+router.get("/:name", (req, res) => {
   res.render("kingdoms/show", {});
 });
-
 
 module.exports = router;
--- services/kingdom-store.js
+++ services/kingdom-store.js
@@ -1,7 +1,29 @@
-const = require('fs');
+const fs = require("fs");
 
-const getJson = () => {
-  const data = fs.readFileSync('kingdoms.json');
+const getData = () => {
+  const data = fs.readFileSync("kingdoms.json");
   const obj = JSON.parse(data);
   return obj;
 };
+
+const getKingdoms = () => {
+  const obj = getData();
+  return obj.kingdoms;
+};
+
+const getCastles = () => {
+  const kingdoms = getKingdoms();
+  return kingdoms.castles;
+};
+
+const getLieges = () => {
+  const castles = getCastles();
+  return castles.lieges;
+};
+
+const getVassals = () => {
+  const lieges = getLieges();
+  return lieges.vassals;
+};
+
+module.exports = { getKingdoms, getCastles, getLieges, getVassals };
