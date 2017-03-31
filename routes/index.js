bb0a3483071c665604bdf29134dee8eed819fc1c
markmarkyesyes <markhahn545@gmail.com>
2017-03-31 11:19:50 -0500

    completed test kingdom json, added incomplete kingdoms view and home router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var kingdoms = require('../lib/kingdoms.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('kingdoms', { kingdoms });
 });
 
 module.exports = router;
