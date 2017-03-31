b24e8356bad242a2283c8d643c73088132971895
tim5046 <tim5046@gmail.com>
2017-03-31 09:44:36 -0700

    list of kingdoms == bueno

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var kingdoms = require('../lib/kingdoms.json')
+var lands = require('../lib/kingdoms.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var kingdoms = {};
+  lands.kingdoms.forEach((kingdom) => {
+    kingdoms[kingdom.name] = {
+      name: kingdom.name,
+      king: kingdom.king,
+      queen: kingdom.queen,
+      castleCount: kingdom.castles.length,
+    }
+  })
   res.render('kingdoms', { kingdoms });
 });
 
 module.exports = router;
+
+
+// list each kingdom with:
+// king name
+// queen name
+// castle count
+
+// king name, queen name, castle count
+
+//
