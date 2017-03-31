f3a5f8c6af4fc2c86c66a681b0a897a5d434de84
deven1 <deven@sloan.mit.edu>
2017-03-31 12:22:10 -0400

    first commit by DB

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const fs = require('fs');
+const express = require("express");
+//const kingdoms = require("./routes/kingdoms");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
-json = fs.readFileSync('./kingdoms.json');
-json = JSON.parse(json);
-json = JSON.stringify(json, null, 2);
+const app = express();
 
-fs.writeFileSync('./kingdoms.json', json);
+const getKingdoms = () => {
+  const data = fs.readFileSync("kingdoms.json");
+  const json = JSON.parse(data);
+  const kingdomsArr = json.kingdoms.map((element) => {
+    return element.name;
+  });
+  console.log(kingdomsArr);
+  //return data;
+};
+
+app.get('/kingdoms', (req, res) => {
+  getKingdoms();
+  res.send('Kingdoms!!');
+
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const hbs = expressHandlebars.create({
+  defaultLayout: "main",
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+//app.use("/kingdoms", castles);
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, () => {
+  console.log("Hey.");
+});
