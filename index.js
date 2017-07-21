59a5431bd7957500cf2f1e4baafa05a6c4ea439c
Kara Thrash <dvdonato@gmail.com>
2017-07-21 14:01:04 -0400

    why do I break the html load

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var kingdomObject = require("./kingdom.json");
 const displayKingdomsObj = require("./displayKingdoms.js");
-const fs = require('fs');
+const fs = require("fs");
 app.set("view engine", "hbs");
 
 const port = process.env.PORT || "3000";
@@ -14,21 +14,35 @@ const port = process.env.PORT || "3000";
 //   next();
 // });
 
-app.get("/", (req, res) => {
-  var htmlData = fs.readFileSync("./index.html", "UTF-8");
-  htmlData = htmlData.replace(
-  "{{ displayHere }}",
-  displayKingdomsObj.returnKingdoms().join());
-  res.end(htmlData);
-  }
-);
+app.use((req, res, next) => {
+  //check kingdom function
+  var urlArray = req.url.split("/");
 
+  console.log("im the first use");
+  if (urlArray[0] != "kingdom") {
+    //next();
+    //send a 404
+    console.log("but I 404");
+  } else {
+    req.requestedkingdom = urlArray[1];
+    next();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.end(htmlData);
+});
 
 app.get("/kingdom/:kingdom/", (req, res) => {
-  let kingdom = req.params["kingdom"];
+  var htmlData = fs.readFileSync("./index.html", "UTF-8");
+  htmlData = htmlData.replace(
+    "{{ displayHere }}",
+    displayKingdomsObj[req.requestedkingdom]
+  );
+  // /let kingdom = req.params["kingdom"];
   //  let castle = req.params[castle];
-  console.log(kingdom);
-  res.end(kingdom);
+  console.log(displayKingdomsObj[req.requestedkingdom] + "test here");
+  res.end(htmlData);
 });
 
 app.listen(port);
