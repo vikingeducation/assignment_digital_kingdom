14591eeb55e3dab129ec1ddc3566eab6234c17ae
Ian Halverson <halversoncode@gmail.com>
2017-07-21 17:20:38 -0400

    built methods to get

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
-const {
-  getKingdoms,
-  getKingdom,
-  getCastles
-} = require("./services/kingdom-store");
+const { getResources, getKingdoms } = require("./services/kingdom-store");
 const bodyParser = require("body-parser");
 
 const port = 3000;
@@ -14,19 +10,30 @@ const host = "localhost";
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static(`${__dirname}/public`));
-//app.use();
+app.all("*", bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  console.log(getKingdoms());
   res.render("kingdoms", { kingdoms: getKingdoms() });
 });
 
 app.get("/:kingdom", (req, res) => {
-  console.log(getKingdom(req.params.kingdom));
-  res.render("kingdom", { kingdom: getKingdom(req.params.kingdom) });
-  console.log(req.params.kingdom);
-  console.log(getKingdom(req.params.kingdom));
-  console.log()
+  res.render("kingdom", getResources(req));
+});
+
+app.get("/:kingdom/castles", (req, res) => {
+  res.render("castles", getResources(req));
+});
+
+app.get("/:kingdom/castles/:castle", (req, res) => {
+  res.render("castle", getResources(req));
+});
+
+app.get("/:kingdom/castles/:castle/lieges", (req, res) => {
+  res.render("lieges", getResources(req));
+});
+
+app.get("/:kingdom/castles/:castle/lieges/:liege", (req, res) => {
+  res.render("liege", getResources(req));
 });
 
 app.listen(port, host, () => {
--- services/kingdom-store.js
+++ services/kingdom-store.js
@@ -1,65 +1,51 @@
 const fs = require("fs");
 const path = require("path");
-const url = require("url");
-//get data
-//readFileSync
-//add data
-//readFileSync
-//save data  //dont export
-//readFileSync
-//delete data
-//readFileSync
 
-const getJsonData = () => fs.readFileSync(path.resolve(__dirname, "../kingdoms.json"), 'utf8')
-
+const getJsonData = () =>
+  fs.readFileSync(path.resolve(__dirname, "../kingdoms.json"), "utf8");
 
 const getKingdoms = () => JSON.parse(getJsonData()).kingdoms;
 
-const getKingdom = (kingdom) => {
-  return getKingdoms().find(ele => ele.name === kingdomName)
+const getKingdom = kingdomName => {
+  return getKingdoms().find(ele => ele.name === kingdomName);
 };
 
-const getCastles = (kingdomName) => {
+const getCastles = kingdomName => {
   return getKingdom(kingdomName).castles;
 };
 
 const getCastle = (kingdomName, castleName) => {
-  return getCastles(kingdomName).find(ele => ele.name === castleName)
-}
+  if (castleName)
+    return getCastles(kingdomName).find(ele => ele.name === castleName);
+};
 
 const getLieges = (kingdomName, castleName) => {
-  return getCastle(kingdomName, castleName).lieges;
+  if (castleName) return getCastle(kingdomName, castleName).lieges;
 };
 
 const getLiege = (kingdomName, castleName, liegeName) => {
-  return getCastle(kingdomName, castleName).find(ele => ele.name === liegeName)
-}
+  if (liegeName)
+    return getLieges(kingdomName, castleName).find(
+      ele => ele.name === liegeName
+    );
+};
 
 const getVassals = (kingdomName, castleName, liegeName) => {
-  return getLiege(kingdomName, castleName, liegeName).vassals;
-}
+  if (liegeName) return getLiege(kingdomName, castleName, liegeName).vassals;
+};
 
-
-const getResource(kingdomName, castleName, liegeName) {
-  const resources = [];
-  resources.push(getKingdom(kingdomName));
-
-  if(castleName) resources.push(get)
-}
-
-// const getResource = (req) => {
-//   let pathname = url.parse(req.url).pathname;
-//   let resourceArray = pathname.split('/');
-//   let data = getKingdoms();
-//
-//   for(let i = 0; i < resourceArray.length; i++) {
-//     for(let j = 0; j < )
-//       data = data[resourceArray[i]];
-//     })
-//   }
+const getResources = req => {
+  var kingdom = req.params.kingdom;
+  var castle = req.params.castle;
+  var liege = req.params.liege;
+  return (resources = {
+    kingdom: getKingdom(kingdom),
+    castle: getCastle(kingdom, castle),
+    liege: getLiege(kingdom, castle, liege)
+  });
+};
 
 module.exports = {
-  getKingdoms,
-  getKingdom,
-
+  getResources,
+  getKingdoms
 };
