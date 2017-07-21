105aaf9050322a14ca015a918f61a0c65e98dba7
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-21 11:39:14 -0500

    added one line of code

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+const { getKingdoms } = require("./services/kingdom-store");
 
 const port = 3000;
 const host = "localhost";
 
 //app.use();
+// app.use(("/", (req, res, next) => {
+//   getKingdoms();
+// })
 
 app.get("/", (req, res) => {
-  res.end("hey!");
+  console.log(getKingdoms());
+  // console.log(getKingdoms())
+  // console.log(getKingdoms);
+  res.end("fjfj");
 });
 
 app.listen(port, host, () => {
--- services/kingdom-store.js
+++ services/kingdom-store.js
@@ -1,4 +1,6 @@
 
+const fs = require('fs');
+const path = require('path');
 //get data
   //readFileSync
 //add data
@@ -8,9 +10,16 @@
 //delete data
   //readFileSync
 
-
-let getKingdoms() {
-
+const getJsonData = () => {
+  let data = fs.readFileSync(path.resolve(__dirname, "kingdoms.json"));
+  console.log(data.toString());
+  //  = fs.readFileSync("kingdoms.json");
+  return data.toString();
 }
 
-let 
+// Just kingdom names (no children info)
+const getKingdoms = () => JSON.parse(getJsonData()).kingdoms.map(ele => ele.name);
+
+
+
+module.exports = {getKingdoms}
