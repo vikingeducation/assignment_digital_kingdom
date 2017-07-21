0af0857d76579bdc692b81e88587eee0ca6b3448
Kara Thrash <dvdonato@gmail.com>
2017-07-21 19:02:41 -0400

    kingdomt ot template doesnts work

--- index.js
+++ index.js
@@ -13,46 +13,49 @@ var htmlData = fs.readFileSync("./index.html", "UTF-8");
 
 app.use((req, res, next) => {
   //check kingdom function
-   urlArray = req.url.split("/");
-  console.log(urlArray);
-  if (urlArray[1] == "kingdom" && urlArray[2] !== undefined) {
+
+  urlArray = req.url.split("/");
+  console.log(urlArray + "url array");
+  if (urlArray[1] === "kingdom" && urlArray[2] !== undefined) {
     req.requestedkingdom = urlArray[2].toLowerCase();
-  } 
-    next();
+  }
+  next();
 });
 
 app.use((req, res, next) => {
   // now check castle
+  console.log(urlArray + "url array");
   if (urlArray[3] == "castle" && urlArray[4] !== undefined) {
     req.requestedCastle = urlArray[4].toLowerCase();
-  } 
-    next();
-  })
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
   console.log("just a normal get");
+
   //for each kingdom
   let kingdomsArray = displayKingdomsObj.returnKingdoms();
 
   res.render("clickableTemplate", { name: kingdomsArray });
-
 });
 
 app.get("/kingdom/:kingdom/", (req, res) => {
+  console.log(req.url);
+
   res.render("DisplayKingdomTemplate", {
-    kingdomName: req.requestedkingdom,
+    kingdomName: req.params.kingdom.toLowerCase(),
     kingName: kingdomObject[req.requestedkingdom]["king"],
     queenName: kingdomObject[req.requestedkingdom]["queen"],
     castles: kingdomObject[req.requestedkingdom]["castles"]
   });
-
 });
 app.get("/kingdom/:kingdom/castle/:castles/", (req, res) => {
-  console.log(res.requestedCastle);
+  console.log(JSON.stringify(req.params, null, 2));
+  console.log(req.requestedkingdom + "req king");
   res.render("DisplayCastleTemplate", {
-    name: req.requestedCastle,
-    liegeName: kingdomObject[req.requestedCastle]["liege"]
+    name: req.params.kingdom
+    //liegeName: kingdomObject[req.requestedCastle]["liege"]
   });
-
 });
 app.listen(port);
