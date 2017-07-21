36fedcb51aa962836fde51ff9922da69c3fa7ca9
Kara Thrash <dvdonato@gmail.com>
2017-07-21 13:14:35 -0400

    change json format

--- displayKingdoms.js
+++ displayKingdoms.js
@@ -1,16 +1,14 @@
 var kingdomObject = require("./kingdom.json");
 
-var kingdomArray = [];
-
 var returnKingdoms = function() {
+  let kingdomArray = [];
+
   for (var k in kingdomObject) {
     // console.log(kingdomObject[k]["king"]);
     // console.log(k);
     kingdomArray.push(k);
-
   }
-  return kingdomArray
-
+  return kingdomArray;
 };
 
-module.exports = {returnKingdoms: returnKingdoms}
+module.exports = { returnKingdoms: returnKingdoms };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var kingdomObject = require("./kingdom.json");
-const DisplayKingdoms = require("./displayKingdoms.js")
+const displayKingdomsObj = require("./displayKingdoms.js");
 app.set("view engine", "hbs");
 
 const port = process.env.PORT || "3000";
@@ -14,17 +14,15 @@ const port = process.env.PORT || "3000";
 // });
 
 app.get("/", (req, res) => {
-  console.log(displayKingdoms.returnKingdoms);
-  res.end(displayKingdoms.returnKingdoms);
+  console.log(displayKingdomsObj.returnKingdoms());
+  res.end(displayKingdomsObj.returnKingdoms().join());
 });
-
 
 app.get("/kingdom/:kingdom/", (req, res) => {
   let kingdom = req.params["kingdom"];
-//  let castle = req.params[castle];
+  //  let castle = req.params[castle];
   console.log(kingdom);
   res.end(kingdom);
 });
-
 
 app.listen(port);
