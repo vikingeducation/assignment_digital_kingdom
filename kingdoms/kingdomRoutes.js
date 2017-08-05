5475c501e0e6b32088c6edc2a6ab306566fb2200
tgturner <turnertgraham@gmail.com>
2017-08-05 15:01:14 -0400

    further into the rabbit hole

--- kingdoms/kingdomRoutes.js
+++ kingdoms/kingdomRoutes.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { getKingdoms, createKingdom } = require('./kingdomService'); 
+const { getKingdoms, createKingdom, getKingdom, createCastle, getCastle } = require('./kingdomService'); 
 
 router.get('/', (req, res) => {
   let kingdoms = getKingdoms();
-  res.render('kingdoms/index', { kingdoms })
+  res.render('kingdoms/index', { kingdoms });
 });
 
 router.post('/', (req, res) => {
-  let name = req.body.name
+  let name = req.body.name;
   createKingdom(name);
   res.redirect('/kingdoms');
+});
+
+router.get('/:name', (req, res) => {
+  let name = req.params.name;
+  let kingdom = getKingdom(name);
+  let castles = kingdom.castles
+  res.render('kingdoms/show', { kingdom, castles })
+});
+
+router.get('/:name/castles/:castleName', (req, res) => {
+  let kingdomName = req.params.name;
+  let castleName = req.params.castleName;
+  let castle = getCastle(kingdomName, castleName);
+  let lieges = castle.lieges
+  res.render('castles/show', { castle, lieges })
+});
+
+router.post('/:name/castles', (req, res) => {
+  let kingdomName = req.params.name;
+  let castleName = req.body.name
+  createCastle(kingdomName, castleName);
+  res.redirect(`/kingdoms/${kingdomName}`);
 });
 
 module.exports = router;
--- kingdoms/kingdomService.js
+++ kingdoms/kingdomService.js
@@ -5,10 +5,31 @@ const getKingdoms = () => {
   return kingdoms.kingdoms
 };
 
+const getKingdom = name => {
+  let kingdoms = _parseAndRead();
+  return _findKingdom(kingdoms, name).kingdom
+};
+
 const createKingdom = name => {
   let kingdoms = _parseAndRead();
   kingdoms.kingdoms.push({kingdomName: name});
   _saveFile(kingdoms);
+};
+
+const createCastle = (kingdomName, castleName) => {
+  let kingdoms = _parseAndRead();
+  let { kingdom, kingdomIndex } = _findKingdom(kingdoms, kingdomName);
+  kingdom.castles = kingdom.castles || [];
+  kingdom.castles.push({ castleName: castleName });
+
+  kingdoms.kingdoms[kingdomIndex] = kingdom;
+  _saveFile(kingdoms)
+};
+
+const getCastle = (kingdomName, castleName) => {
+  let kingdoms = _parseAndRead();
+  let kingdom = _findKingdom(kingdoms, kingdomName).kingdom;
+  return _findCastle(kingdom, castleName).castle
 };
 
 const _parseAndRead = () => {
@@ -18,9 +39,30 @@ const _parseAndRead = () => {
 
 const _saveFile = json => {
   fs.writeFileSync('./data.json', JSON.stringify(json, null, 4));
-}
+};
+
+const _findKingdom = (kingdoms, name) => {
+  let kingdom = kingdoms.kingdoms.filter(kingdom => { 
+    return kingdom.kingdomName === name
+  })[0];
+
+  let index = kingdoms.kingdoms.indexOf(kingdom);
+  return { index, kingdom };
+};
+
+const _findCastle = (kingdom, name) => {
+  let castle = kingdom.castles.filter(castle => {
+    return castle.castleName === name
+  })[0];
+
+  let index = kingdom.castles.indexOf(castle);
+  return { index, castle };
+};
 
 module.exports = {
   getKingdoms,
-  createKingdom
+  createKingdom,
+  getKingdom,
+  createCastle,
+  getCastle
 }
