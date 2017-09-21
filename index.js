f31bbedc2d5dc933b410866bb71a2ed0765caabb
Coelacanth7 <galford151@gmail.com>
2017-09-21 00:54:15 -0700

    got some json data sending to webpage

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const jsonModule = require("./services/kingdom-store");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-	res.end("hey");
+	var json = jsonModule.readJson();
+	var kingdoms = jsonModule.getKingdoms();
+	res.send(kingdoms);
+});
+
+const hbs = expressHandlebars.create({
+	defaultLayout: "main"
 });
 
 //server
---
+++ services/kingdom-store.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const jsonModule = {};
+
+jsonModule.readJson = () => {
+	const data = fs.readFileSync("kingdom.json");
+	const json = JSON.parse(data);
+	return json;
+};
+
+jsonModule.getKingdoms = () => {
+	const json = jsonModule.readJson();
+	const kingdoms = Object.keys(json);
+	return kingdoms;
+};
+
+module.exports = jsonModule;
