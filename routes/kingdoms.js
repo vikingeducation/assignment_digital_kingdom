6d9864d2f0db2389a267a42dc8cedfc9bc78d2cd
promilo <milind.shah@mail.utoronto.ca>
2017-10-10 22:00:29 -0400

    finished adding kingdom functionality

--- routes/kingdoms.js
+++ routes/kingdoms.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const {
   getKingdoms,
+  addKingdom,
+  getCastles,
+  getVassals
 } = require("../services/kingdoms")
 
 router.get("/", (req, res) => {
@@ -11,7 +14,19 @@ router.get("/", (req, res) => {
   res.render("kingdoms", { kingdoms })
 })
 
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  const king = req.body.king;
+  const queen = req.body.queen;
+  addKingdom(name, king, queen);
+  res.redirect("back");
+});
+
 router.get("/:kingdom", (req, res) => {
+  const kingdom = getCastles(req.params.kingdom)
+  console.log("kingdom", kingdom)
+  res.render('kingdom', kingdom)
+
 })
 
 router.get("/:kingdom/:castles", (req, res) =>{
--- services/kingdoms.js
+++ services/kingdoms.js
@@ -6,12 +6,49 @@ const getJson = () => {
   return json;
 };
 
+const saveJson = json => {
+  fs.writeFileSync("kingdoms.json", JSON.stringify(json, null, 4));
+};
+
 const getKingdoms = () => {
   const json = getJson();
-  const kingdoms = Object.keys(json.kingdom);
+  const kingdoms = json.kingdom;
   return kingdoms;
 };
 
+const addKingdom =(name, king, queen) => {
+  const json = getJson();
+  if (json.kingdom[name]) return;
+  json.kingdom.push({
+    name,
+    king,
+    queen,
+    "castles": []
+  });
+  saveJson(json);
+}
+
+const getCastles = (kngdm) => {
+  const json = getJson()
+  const castles = json.kingdom[kngdm];
+  return castles;
+}
+
+const getLieges = (kingdom, castle) => {
+  const castlesData = getCastles(kingdom);
+  const vassals = Object.keys(castlesData[castle])
+  return vassals
+}
+
+const getVassals = (kingdom, castle) => {
+  const castlesData = getCastles(kingdom);
+  const vassals = Object.keys(castlesData[castle])
+  return vassals
+}
+
 module.exports = {
-  getKingdoms
+  getKingdoms,
+  addKingdom,
+  getCastles,
+  getVassals
 }
