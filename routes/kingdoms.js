11adac999e119b23a525f58a44f7d563c32d21f0
William Huynh <31399549+wdh7@users.noreply.github.com>
2017-10-14 15:20:57 -0700

    Add view for each kingdom

--- routes/kingdoms.js
+++ routes/kingdoms.js
@@ -61,13 +61,70 @@ router.post('/new', (req, res) => {
 });
 
 
+router.get('/:id', (req, res) => {
+  const kingdomIdx = req.params.id;
+  const path = './data/kingdoms.json';
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    data = data.toString();
+    data = JSON.parse(data);
+
+    const kingdom = data.kingdoms[kingdomIdx];
+
+    // show name of each castle in kingdom
+    // show # of lieges in each castle
+    const castlesArr = kingdom.castles;
+    res.render('kingdom', {
+      kingdomIdx: kingdomIdx,
+      kingdomName: kingdom.name,
+      castles: castlesArr
+    });
 
 
+  });
+
+});
 
 
+router.post('/:id/edit', (req, res) => {
+  // edit the kingdom by adding new castle
+  const kingdomIdx = req.params.id;
+  const castleName = req.body.castleName;
+  const path = './data/kingdoms.json';
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    data = data.toString();
+    data = JSON.parse(data);
+
+    const newCastle = {
+      name: castleName
+    };
+
+    data.kingdoms[kingdomIdx].castles.push(newCastle);
+
+    fs.writeFile(path, JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        throw err;
+      }
+
+      res.render('kingdom', {
+        kingdomName: data.kingdoms[kingdomIdx].name,
+        castles: data.kingdoms[kingdomIdx].castles,
+        kingdomIdx: kingdomIdx
+      })
+    })
 
 
-
+  })
+});
 
 
 
