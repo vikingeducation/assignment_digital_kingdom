225a7adef2f411c573ad3544d9b0668c92fbe7ba
triflegated <adatepe.justin@gmail.com>
2017-11-17 16:36:53 -0500

    adding kings queens and removing links from lieges

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,25 @@ function getCurrentState(paramObj){
 	let returnArray;
 	let keys;
 	let displayObject;
+	let displayArray;
+
 	if(!paramObj.kingdom){
 		keys = Object.keys(hierarchy.kingdoms)
 		displayObject = hierarchy.kingdoms;
+		returnArray = keys.map(x => {
+			displayObject[x].display = [`King: ${hierarchy.kings[displayObject[x].kingId].name}`, 
+			`Queen: ${hierarchy.queens[displayObject[x].queenId].name}`, 
+			`Number of Castles: ${displayObject[x].castleIds.length}`]
+			return displayObject[x];
+	});
+		
 	} else if(!paramObj.castle){
 		keys =  hierarchy.kingdoms[paramObj.kingdom].castleIds;
 		displayObject = hierarchy.castles;
+		returnArray = keys.map(x => {
+			displayObject[x].display = hierarchy.lieges[displayObject[x].liegeIds].map( y => {
+				return `${hierarchy.lieges[y].name}  Number of vassals: ${hierarchy.lieges[y].vassalIds.length}`;
+			})}); 
 	} else if(!paramObj.liege){
 		keys = hierarchy.castles[paramObj.castle].liegeIds;
 		displayObject = hierarchy.lieges;
@@ -19,9 +32,9 @@ function getCurrentState(paramObj){
 		keys = hierarchy.lieges[paramObj.liege].vassalIds;
 		displayObject = hierarchy.vassals;
 	}
-	returnArray = keys.map(x => {
-		return displayObject[x];
-	});
+	
+
+
 	return returnArray;
 }
 
