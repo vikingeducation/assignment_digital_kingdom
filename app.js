8608171350bb278989ffe67c61fefe0a67ded586
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-17 10:34:57 -0800

    loaded main data

--- app.js
+++ app.js
@@ -25,10 +25,53 @@ const getKingdomNames = kingdomsObject => {
   return names;
 };
 
-const kingdomNames = getKingdomNames(getKingdomsObject());
+const getKingNames = (kingdomsObject) => {
+  let kingIds = []
+  for (kingdom in kingdomsObject) {
+    kingIds.push(kingdomsObject[kingdom]["kingId"]);
+  }
+  const data = fs.readFileSync("./data/kings.json");
+  const json = JSON.parse(data);
+  return kingIds.map(id => json[id]["name"] )
+}
+
+const getQueenNames = (kingdomsObject) => {
+  let queenIds = []
+  for (kingdom in kingdomsObject) {
+    queenIds.push(kingdomsObject[kingdom]["queenId"]);
+  }
+  const data = fs.readFileSync("./data/queens.json");
+  const json = JSON.parse(data);
+  return queenIds.map(id => json[id]["name"] )
+}
+
+const getCastleNumber = (kingdomsObject) => {
+  let castleNumber = []
+  for (kingdom in kingdomsObject) {
+    castleNumber.push(kingdomsObject[kingdom]["castleIds"].length);
+  }
+  return castleNumber
+}
+
+const kingdomsObject = getKingdomsObject()
+const kingdomNames = getKingdomNames(kingdomsObject);
+const kingNames = getKingNames(kingdomsObject)
+const queenNames = getQueenNames(kingdomsObject)
+const castleNumber = getCastleNumber(kingdomsObject)
+
+console.log(castleNumber);
+/*
+const getKingName = (kingdomNames, KingdomsObject) => {
+  KingdomsObject[KingdomID]
+}
+
+const kingName;
+*/
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("kingdoms", { kingdomNames: kingdomNames });
