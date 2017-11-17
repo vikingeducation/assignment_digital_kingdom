e4e48dc276632046fa878d02df98b097556398cf
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-17 11:52:00 -0600

    tomatoes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+let heirarchy = require('../lib/extract');
 
+let getCurrentState(paramObj){
+	if(!paramObj.kingdoms){
+		return paramObj.kingdoms;
+	} else if(!paramObj.castles){
+		return paramObj.castles;
+	} else if(!paramObj.lieges){
+		return paramObj.lieges;
+	} else {
+		return paramObj.kingdoms;
+	}
+}
+///:kingdom/:castle/:liege
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	const currentState = getCurrentState(req.params);
   res.render('index', { title: 'Express' });
 });
 
