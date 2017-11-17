9be1005dcbbf1902c0c81bf8961894803f0c9e70
triflegated <adatepe.justin@gmail.com>
2017-11-17 13:29:52 -0500

    moved routing functionality out of app.js into routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 let heirarchy = require('../lib/extract');
 
-let getCurrentState(paramObj){
+function getCurrentState(paramObj){
 	if(!paramObj.kingdoms){
 		return paramObj.kingdoms;
 	} else if(!paramObj.castles){
@@ -13,11 +13,31 @@ let getCurrentState(paramObj){
 		return paramObj.kingdoms;
 	}
 }
+
+
+
 ///:kingdom/:castle/:liege
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	const currentState = getCurrentState(req.params);
-  res.render('index', { title: 'Express' });
+	console.log(currentState);
+  res.render('index', { items: currentState });
 });
+
+router.get('/:kingdom', index) {
+	const currentState = getCurrentState(req.params);
+	console.log(currentState);
+  res.render('index', { items: currentState });
+}
+;
+router.get('/:kingdom/:castle', index);
+router.get('/:kingdom/:castle/:liege', index);
+
+/*
+router.get('/:title', function(req, res, next) {
+  console.log(req.params);
+  res.render('index', { title: req.params });
+});
+*/
 
 module.exports = router;
