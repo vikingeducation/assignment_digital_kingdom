eb4c347471101da76b2672a98809103ca3073c96
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-17 12:58:34 -0600

    template works

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,66 @@
 var express = require('express');
 var router = express.Router();
-let heirarchy = require('../lib/extract');
+let hierarchy = require('../lib/extract');
 
 function getCurrentState(paramObj){
-	if(!paramObj.kingdoms){
-		return paramObj.kingdoms;
-	} else if(!paramObj.castles){
-		return paramObj.castles;
-	} else if(!paramObj.lieges){
-		return paramObj.lieges;
-	} else {
-		return paramObj.kingdoms;
+	let returnArray;
+	if(!paramObj.kingdom){
+		displayObject = hierarchy.kingdoms;
+	} else if(!paramObj.castle){
+		displayObject = hierarchy.castles;
+	} else if(!paramObj.liege){
+		displayObject = hierarchy.lieges;
+	}else {
+		displayObject = hierarchy.vassals;
 	}
+	returnArray = Object.keys(displayObject).map(x => {
+		return displayObject[x];
+	});
+	return returnArray;
 }
 
 
 
 ///:kingdom/:castle/:liege
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	const currentState = getCurrentState(req.params);
-	console.log(currentState);
+/*router.get('/', function(req, res, next) {
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
   res.render('index', { items: currentState });
 });
 
-router.get('/:kingdom', index) {
-	const currentState = getCurrentState(req.params);
-	console.log(currentState);
+router.get('/:kingdom', function(req, res, next) {
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
   res.render('index', { items: currentState });
-}
-;
-router.get('/:kingdom/:castle', index);
-router.get('/:kingdom/:castle/:liege', index);
+});
+*/
+router.get('/:kingdom/:castle/:liege', function(req, res, next){
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
+	console.log(currentState)
+  res.render('index', { items: currentState });
+});
+router.get('/:kingdom/:castle', function(req, res, next) {
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
+  res.render('index', { items: currentState });
+});
+router.get('/:kingdom', function(req, res, next) {
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
+  res.render('index', { items: currentState });
+});
+router.get('/', function(req, res, next) {
+	let currentState = getCurrentState(req.params);
+	console.log(req.params);
+  res.render('index', { items: currentState });
+});
 
-/*
 router.get('/:title', function(req, res, next) {
   console.log(req.params);
   res.render('index', { title: req.params });
 });
-*/
+
 
 module.exports = router;
