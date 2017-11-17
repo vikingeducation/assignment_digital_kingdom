5537c5ed26689f8ddccd0c3447c64241b127f65e
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-17 17:08:22 -0500

    switch

--- routes/kingdoms.js
+++ routes/kingdoms.js
@@ -1,44 +1,56 @@
-"use strict";
+'use strict';
 
-const Express = require("express");
+const Express = require('express');
 const router = Express.Router();
-const Kingdoms = require("../services/kingdom_store.js");
-const fs = require("fs");
-const kingdomsObject = Promise.resolve(require("../data/kingdoms.json"));
-const kingsObject = Promise.resolve(require("../data/kings.json"));
-const queensObject = Promise.resolve(require("../data/queens.json"));
-const castlesObject = Promise.resolve(require("../data/castles.json"));
-const liegesObject = Promise.resolve(require("../data/lieges.json"));
-const vassalsObject = Promise.resolve(require("../data/vassals.json"));
+const Kingdoms = require('../services/kingdom_store.js');
+const fs = require('fs');
+const kingdomsObject = Promise.resolve(require('../data/kingdoms.json'));
+const kingsObject = Promise.resolve(require('../data/kings.json'));
+const queensObject = Promise.resolve(require('../data/queens.json'));
+const castlesObject = Promise.resolve(require('../data/castles.json'));
+const liegesObject = Promise.resolve(require('../data/lieges.json'));
+const vassalsObject = Promise.resolve(require('../data/vassals.json'));
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   //let kingdoms = Kingdoms.getKingdoms().then(console.log);
   Kingdoms.callArray();
   //let kingdoms = Kingdoms.getKingdoms();
   //res.send();
 });
 
-router.get("/kingdoms", (req, res) => {
+router.get('/kingdoms', (req, res) => {
+  let kingdoms;
   kingdomsObject
-    .then(kingdoms => res.render("homepage", { kingdoms }))
-    .catch(err => res.render("error", { err }));
+    .then(data => {
+      kingdoms = data;
+      return kingsObject;
+    })
+    .then(data => {
+      kingdoms['1'].king = data['1'].name;
+      return queensObject;
+    })
+    .then(data => {
+      kingdoms['1'].queen = data['1'].name;
+      res.render('homepage', { kingdoms });
+    })
+    .catch(err => res.render('error', { err }));
 });
 
-router.get("/kingdoms/:kingdom", (req, res) => {});
-
-router.get("/kingdoms/:kingdom/castles/:castle", (req, res) => {});
-
-router.get(
-  "/kingdoms/:kingdom/castles/:castle/lieges/:liege",
-  (req, res) => {}
-);
-
-router.get(
-  "/kingdoms/:kingdom/castles/:castle/lieges/:lieges/vassals/:vassal",
-  (req, res) => {}
-);
-
-const pullData = () => {};
+// router.get('/kingdoms/:kingdom', (req, res) => {});
+//
+// router.get('/kingdoms/:kingdom/castles/:castle', (req, res) => {});
+//
+// router.get(
+//   '/kingdoms/:kingdom/castles/:castle/lieges/:liege',
+//   (req, res) => {}
+// );
+//
+// router.get(
+//   '/kingdoms/:kingdom/castles/:castle/lieges/:lieges/vassals/:vassal',
+//   (req, res) => {}
+// );
+//
+// const pullData = () => {};
 
 module.exports = router;
 
