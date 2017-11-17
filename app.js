bd28cefafc12b2084ab2b700e6956dbc1e41e1e0
edwinyung <eyung7@gmail.com>
2017-11-17 12:41:40 -0500

    kingdoms

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 //var data = require('./data');
-var fs =require('fs');
-var http = require('http');
-var express = require('express');
-var router = require('./router.js');
+var fs = require("fs");
+var http = require("http");
+var express = require("express");
+var router = require("./router.js");
 const app = express();
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res)=>{
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/kingdoms", (req, res) => {
   var kingdoms = router.getKingdoms();
-  res.send(kingdoms);
-})
+  let kingdoms = {
+    keys: Object.keys(kingdoms),
+    kingdoms: kingdoms
+  };
+  console.log(localObj.kingdoms);
+  res.render("kingdoms", localObj);
+});
 
-app.listen(3000, 'localhost', ()=>{
-  console.log('app is listening');
-})
+app.listen(3000, "localhost", () => {
+  console.log("app is listening");
+});
